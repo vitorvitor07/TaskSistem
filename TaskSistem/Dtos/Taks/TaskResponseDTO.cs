a3186d96199cc72f62f8e16e8fe411e5c1939725
vitorvitor07 <vitorsilva_53@hotmail.com>
2026-06-19 17:44:31 -0300

    adding docker ingra

---
+++ TaskSistem/Dtos/Taks/TaskResponseDTO.cs
@@ -0,0 +1,17 @@
+﻿
+using TaskSistem.Dtos.User;
+
+namespace TaskSistem.DTOs.Task
+{
+    public class TaskToResponseDTO
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public int Status { get; set; }
+        public int UserId { get; set; }
+
+        // Em vez de UserModel, usa o DTO limpo!
+        public UserToResponseDto? User { get; set; }
+    }
+}
---
+++ TaskSistem/Dtos/User/UserToResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TaskSistem.Dtos.User
+{
+    public class UserToResponseDto
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+    }
+}
